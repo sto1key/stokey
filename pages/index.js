e4cc8292d426ab2d452cd54a0bb8017f80c4ae3f
stokey <82271266+sto1key@users.noreply.github.com>
2023-09-10 20:12:27 +0000

    feat: add content into home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
+import styles from "../styles/home.module.css"
+
 export default function Home() {
-  return <h1>Hello world 🗝️</h1>;
+  return (
+    <div
+      className={styles.container}
+    >
+      <h1>stokey</h1>
+      <ul>
+        <li>
+          <a href="https://www.youtube.com/@sto1key" target="_blank">Canal YouTube</a>
+        </li>
+        <li>
+          <a href="https://discord.gg/KxuYpY8RFx" target="_blank">Comunidade Discord</a>
+        </li>
+        <li>
+          <a href="https://blog.stokey.com.br/" target="_blank">Blog</a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/sto1key/" target="_blank">Instagram</a>
+        </li>
+        
+      </ul>
+    </div>
+  );
 }
